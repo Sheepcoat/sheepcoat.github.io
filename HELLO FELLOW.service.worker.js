689661b4f56a02a96d0657bf11441d99d250475d
Sheepcoat <ambmoura@live.com.pt>
2023-04-23 22:34:52 +0100

    beta2

--- HELLO FELLOW.service.worker.js
+++ HELLO FELLOW.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1682283369|905678604";
+const CACHE_VERSION = "1682285426|22794231";
 const CACHE_PREFIX = "HELLO FELLOW-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "HELLO FELLOW.offline.html";
