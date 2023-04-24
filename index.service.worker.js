137db2ff8e6006b81c11151a1ca62fe1883ebdd2
Sheepcoat <ambmoura@live.com.pt>
2023-04-24 02:42:00 +0100

    im gonna play 2 losses

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1682299650|2999951152";
+const CACHE_VERSION = "1682300472|3822036207";
 const CACHE_PREFIX = "HELLO FELLOW-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
