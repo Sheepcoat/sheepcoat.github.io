cdbc9cef692c35573cec99d9d7497155a639e1cf
Sheepcoat <ambmoura@live.com.pt>
2023-04-24 21:19:01 +0100

    iphone ready

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1682353841|478193510";
+const CACHE_VERSION = "1682367480|718287677";
 const CACHE_PREFIX = "HELLO FELLOW-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
