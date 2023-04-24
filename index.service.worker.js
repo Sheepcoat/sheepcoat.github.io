a7814a04a39ee75ba2a37e40e387e0e9816b71a1
Sheepcoat <ambmoura@live.com.pt>
2023-04-24 01:58:47 +0100

    sdk

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1682294403|885599653";
+const CACHE_VERSION = "1682297877|1226868172";
 const CACHE_PREFIX = "HELLO FELLOW-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
