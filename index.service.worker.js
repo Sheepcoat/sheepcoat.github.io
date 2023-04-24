b7cd1fa2d9fd222a69311bd4b42515a04eca3615
Sheepcoat <ambmoura@live.com.pt>
2023-04-24 06:37:32 +0100

    gpt help

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1682313928|2237864155";
+const CACHE_VERSION = "1682314393|2702935005";
 const CACHE_PREFIX = "HELLO FELLOW-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
