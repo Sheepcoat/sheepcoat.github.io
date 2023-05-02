260ab288611f3d1511963e779eaa6d56e7ac4c40
Sheepcoat <ambmoura@live.com.pt>
2023-05-02 15:41:20 +0100

    big letters, big player

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1682956813|3068255414";
+const CACHE_VERSION = "1683038452|3206617929";
 const CACHE_PREFIX = "HELLO FELLOW-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
