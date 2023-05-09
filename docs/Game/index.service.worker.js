36eb0dddf9abf6f3f29dae9b27885214e30915c5
Sheepcoat <ambmoura@live.com.pt>
2023-05-09 14:14:39 +0100

    2link test

--- docs/Game/index.service.worker.js
+++ docs/Game/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1683632792|5763459708";
+const CACHE_VERSION = "1683637950|10921954675";
 const CACHE_PREFIX = "HELLO FELLOW-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
--- docs/Markl/index.service.worker.js
+++ docs/Markl/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1683633344|6315615180";
+const CACHE_VERSION = "1683638040|11011922085";
 const CACHE_PREFIX = "HELLO FELLOW-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
